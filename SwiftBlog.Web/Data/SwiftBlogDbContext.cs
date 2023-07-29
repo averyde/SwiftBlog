22809107240b09b17389a9b2cfe82639f55a5d44
AveryDeemer <averydeemer@gmail.com>
2023-07-29 19:25:38 -0400

    Add project files.

---
+++ SwiftBlog.Web/Data/SwiftBlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SwiftBlog.Web.Models.Domain;
+
+namespace SwiftBlog.Web.Data
+{
+    public class SwiftBlogDbContext : DbContext
+    {
+        public SwiftBlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
